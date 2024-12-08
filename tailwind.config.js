5557267e511e27b8b37a44039a1f132d4605d61f
liann-melanny-huaman-cunyas <melanycunyas@gmail.com>
2024-12-08 12:22:51 -0500

    vite2

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,7 @@ export default {
         "./resources/**/**/*.blade.php",
         "./resources/**/*.js",
         "./resources/**/*.vue",
+        "'./resources/css/**/*.css',"
     ],
 
     theme: {
