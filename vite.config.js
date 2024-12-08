79b8f8e959d1fc6dd9c14c934b2d0c092888444f
liann-melanny-huaman-cunyas <melanycunyas@gmail.com>
2024-12-08 12:35:02 -0500

    vite 3

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
                 'resources/js/app.js',
                 'resources/css/custom.css',
             ],
+            buildDirectory: 'build',
             refresh: true,
         }),
     ],
